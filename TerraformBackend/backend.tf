5addcc9d1e75a5adc7163aeb594a61ac368750e3
Ngozi Nwadialo <jonathanengee@gmail.com>
2026-04-15 17:00:21 +0100

    Update backend.tf

--- TerraformBackend/backend.tf
+++ TerraformBackend/backend.tf
@@ -87,5 +87,5 @@ variable "bucket_name" {
 variable "dynamodb_table_name" {
     description = "Name of the DynamoDB table for state locking"
     type        = string
-    default     = "ecommerce-terraform-locks"
+    default     = "ecommerce-terraform-locks-engee"
 }
